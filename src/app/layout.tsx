9838a936e7c4700af061655a045f2d3d8bbd2f1e
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2025-04-04 00:29:03 +0600

    sidebar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SessionProviderWrapper from "./_components/SessionProviderWrapper";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <SessionProviderWrapper>
+          <SidebarProvider>
+
+            <AppSidebar />
+           
+              <SidebarTrigger />
+              {children}
+      
+          </SidebarProvider>
+        </SessionProviderWrapper>
       </body>
     </html>
   );
